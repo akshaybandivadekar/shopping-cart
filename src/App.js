4663327856e24a0bb9602077a5a60b36cd150d99
Akshay Bandivadekar <c4.akshay.bandivadekar@gmail.com>
2022-09-06 22:13:25 +0530

    Creating Pages Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
+import Cart from './components/Cart';
 
 function App() {
   return (
-    <div className='App'>
+    <BrowserRouter>
       <Header />
-    </div>
+      <div>
+        <Routes>
+          <Route path='/' element={<Home />} exact />
+
+          <Route path='/cart' element={<Cart />} exact />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
